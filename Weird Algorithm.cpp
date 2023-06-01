62f63601166207d2c2aa366dafe40e84e4990e01
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-06-01 19:28:40 +0530

    Create Weird Algorithm.cpp

---
+++ Weird Algorithm.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << n << " ";
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+        }
+        else
+        {
+            n = 3 * n + 1;
+        }
+        cout << n << " ";
+    }
+    cout << endl;
+}
